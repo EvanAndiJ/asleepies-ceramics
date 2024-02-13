eb67acb66a1c1935b19f8ada4b3be0b09ca92532
Evan Jones <evanandyjones@gmail.com>
2024-02-13 00:35:07 -0800

    path fix

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} >
+      <Route path="/asleepies-ceramics" element={<App />} >
         <Route index element={<FeaturedItems />}/>
         <Route index element={<Shop />}/>
 
