c2aa65815a97d4902fea6f4c57ea669717dc8304
Evan Jones <evanandyjones@gmail.com>
2024-02-13 00:27:08 -0800

    Updates

---
+++ data/sample-data.js
@@ -0,0 +1,24 @@
+const products = {
+    pot001: { sku: 'pot001', price:10, stock:1, title: 'A Pot', desc: 'This is a pot', cat: 'pot', images: [],created: Date.now(),updated: Date.now()},
+    pot002: { sku: 'pot002', price:10, stock:1, title: 'A Pot', desc: 'This is a pot', cat: 'pot', images: [],created: Date.now(),updated: Date.now()},
+    pot003: { sku: 'pot003', price:10, stock:1, title: 'A Pot', desc: 'This is a pot', cat: 'pot', images: [],created: Date.now(),updated: Date.now()},
+    pot004: { sku: 'pot004', price:10, stock:1, title: 'A Pot', desc: 'This is a pot', cat: 'pot', images: [],created: Date.now(),updated: Date.now()},
+    bwl001: { sku: 'bwl001', price:10, stock:1, title: 'A Bowl', desc: 'This is a bowl', cat: 'bwl', images: [],created: Date.now(),updated: Date.now()},
+    bwl002: { sku: 'bwl002', price:10, stock:1, title: 'A Bowl', desc: 'This is a bowl', cat: 'bwl', images: [],created: Date.now(),updated: Date.now()},
+    bwl003: { sku: 'bwl003', price:10, stock:1, title: 'A Bowl', desc: 'This is a bowl', cat: 'bwl', images: [],created: Date.now(),updated: Date.now()},
+    bwl004: { sku: 'bwl004', price:10, stock:1, title: 'A Bowl', desc: 'This is a bowl', cat: 'bwl', images: [],created: Date.now(),updated: Date.now()},
+    mug001: { sku: 'mug001', price:10, stock:1, title: 'A Mug', desc: 'This is a mug', cat: 'bwl', images: [],created: Date.now(),updated: Date.now()},
+    mug002: { sku: 'mug002', price:10, stock:1, title: 'A Mug', desc: 'This is a mug', cat: 'bwl', images: [],created: Date.now(),updated: Date.now()},
+    mug003: { sku: 'mug003', price:10, stock:1, title: 'A Mug', desc: 'This is a mug', cat: 'bwl', images: [],created: Date.now(),updated: Date.now()},
+    mug004: { sku: 'mug004', price:10, stock:1, title: 'A Mug', desc: 'This is a mug', cat: 'bwl', images: [],created: Date.now(),updated: Date.now()},
+}
+
+const orders = {}
+
+const orderHolds = {}
+
+const users = {}
+
+export default data = {
+    products
+}
